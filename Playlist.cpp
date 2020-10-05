585121f380e33f61fc20b653dbb412d9c0d033ad
Vinay Srinivasan <zapeprwing@gmail.com>
2020-10-05 16:33:27 -0700

    Completed all function implementations in Playlist.cpp

--- Playlist.cpp
+++ Playlist.cpp
@@ -74,3 +74,153 @@ void Playlist::AddSong(string id, string songName, string artistName, int length
     }
 }
 
+bool Playlist::RemoveSong(string id) {
+   if (head == NULL) {
+       cout << "Playlist is empty." << endl;
+       return false;
+   }
+
+   PlaylistNode* curr = head;
+   PlaylistNode* prev = NULL;
+   while (curr != NULL) {
+       if (curr->getID() == id) {
+           break;
+       }
+       prev = curr;
+       curr = curr->GetNext();
+   }
+
+   if (curr == NULL) {
+       cout << "\"" << curr->getSong() << "\" is not found" << endl;
+       return false;
+   }
+   else {
+       if (prev != NULL) {
+           prev->SetNext(curr->GetNext());
+       }
+       else {
+           head = curr->GetNext();
+       }
+
+       if (tail == curr) {
+           tail = prev;
+       }
+   
+       cout << "\"" << curr->getSong() << "\" removed" << endl;
+       delete curr;
+       return true;
+   }
+}
+
+bool Playlist::ChangePosition(int oldPos, int newPos) {
+   if (head == NULL) {
+       cout << "Playlist is empty." << endl;
+       return false;
+   }
+
+
+   PlaylistNode* prev = NULL;
+   PlaylistNode* curr = head;
+
+   int pos;
+   if (head == NULL || head == tail) {
+       return false;
+   }
+
+   for (pos = 1; curr != NULL && pos < oldPos; pos++) {
+       prev = curr;
+       curr = curr->GetNext();
+   }
+   if (curr != NULL) {
+       string currentSong = curr->getSong();
+
+       if (prev == NULL) {
+           head = curr->GetNext();
+       }
+       else {
+           prev->SetNext(curr->GetNext());
+       }
+
+       if (curr == tail) {
+           tail = prev;
+       }
+
+       PlaylistNode* curr1 = curr;
+       prev = NULL;
+       curr = head;
+       //now locate the new position
+       for (pos = 1; curr != NULL && pos < newPos; pos++) {
+           prev = curr;
+           curr = curr->GetNext();
+       }
+
+       //insert in located position
+
+       if (prev == NULL) {
+           curr1->SetNext(head);
+           head = curr1;
+       }
+       else {
+           curr1->InsertAfter(prev);
+       }
+
+       if (curr == NULL) {
+           tail = curr1;
+       }
+
+
+       cout << "\"" << currentSong << "\" moved to position " << newPos << endl;
+       return true;
+
+   }
+   else {
+       cout << "Song's current position is invalid" << endl;
+       return false;
+   }
+}
+
+void Playlist::SongsByArtist(string artist) {
+   if (head == NULL) {
+       cout << "Playlist is empty." << endl;
+   }
+   else {
+       PlaylistNode* curr = head;
+       int i = 1;
+       while (curr != NULL) {
+           if (curr->getArtist() == artist) {
+               cout << endl << i << "." << endl;
+               curr->PrintPlaylistNode();              
+           }
+           curr = curr->GetNext();
+           i++;
+       }
+   }
+}
+
+int Playlist::totalTime() {
+   int total = 0;
+   PlaylistNode* curr = head;
+
+   while (curr != NULL) {
+       total += curr->getLength();
+       curr = curr->GetNext();
+   }
+
+   return total;
+}
+
+
+void Playlist::PrintList() {
+   if (head == NULL) {
+       cout << "Playlist is empty." << endl;
+   }
+   else {
+       PlaylistNode* curr = head;
+       int i = 1;
+       while (curr != NULL) {
+           cout << endl << i++ << "." << endl;
+           curr->PrintPlaylistNode();          
+           curr = curr->GetNext();
+       }
+   }
+}
