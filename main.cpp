3e4cb82203ed9c51e62ef489d1d93640d1bf711b
Vinay Srinivasan <zapeprwing@gmail.com>
2020-10-06 08:59:38 -0700

    finished

--- main.cpp
+++ main.cpp
@@ -1,9 +1,99 @@
-#include <iostream>
 #include "Playlist.h"
-
+#include <iostream>
 using namespace std;
 
+void PrintMenu(string title);
+
 int main() {
-    cout << "Hello World!" << endl;
-    return 0;
+   string playlistTitle;
+   cout << "Enter playlist's title: ";
+   getline(cin, playlistTitle);
+
+   PrintMenu(playlistTitle);  
+  
+   return 0;
 }
+
+
+void PrintMenu(string title) {
+   Playlist list;
+   string id;
+   string nameOfSong;
+   string nameOfArtist;
+   int length;
+   int oldPos;
+   int newPos;
+   char choice;
+
+   while (true)
+   {
+       cout << endl << title << " PLAYLIST MENU" << endl;
+       cout << "a - Add song" << endl;
+       cout << "d - Remove song" << endl;
+       cout << "c - Change position of song" << endl;
+       cout << "s - Output songs by specific artist" << endl;
+       cout << "t - Output total time of playlist (in seconds)" << endl;
+       cout << "o - Output full playlist" << endl;
+       cout << "q - Quit" << endl << endl;
+
+       cout << "Choose an option: ";
+       cin >> choice;
+       cin.ignore(); //flush newline
+
+       if (choice == 'q' || choice == 'Q')
+       {
+           exit(1);
+       }
+       else if (choice == 'a' || choice == 'A')
+       {
+           cout << "\nADD SONG" << endl;
+           cout << "Enter song's unique ID: ";
+           cin >> id;
+           cin.ignore();//ignore newline
+           cout << "Enter song's name: ";
+           getline(cin, nameOfSong);
+           cout << "Enter artist's name: ";
+           getline(cin, nameOfArtist);
+           cout << "Enter song's length (in seconds): ";
+           cin >> length;
+           list.AddSong(id, nameOfSong, nameOfArtist, length);
+       }
+       else if (choice == 'd' || choice == 'D')
+       {
+           cout << "\nREMOVE SONG" << endl;
+           cout << "Enter song's unique ID: ";
+           cin >> id;
+           list.RemoveSong(id);
+       }
+       else if (choice == 'c' || choice == 'C')
+       {
+           cout << "\nCHANGE POSITION OF SONG" << endl;
+           cout << "Enter song's current position: ";
+           cin >> oldPos;
+           cout << "Enter new position for song: ";
+           cin >> newPos;
+           list.ChangePosition(oldPos, newPos);
+       }
+       else if (choice == 's' || choice == 'S')
+       {
+           cout << "\nOUTPUT SONGS BY SPECIFIC ARTIST" << endl;
+           cout << "Enter artist's name: ";
+           getline(cin, nameOfArtist);          
+           list.SongsByArtist(nameOfArtist);
+       }
+       else if (choice == 't' || choice == 'T')
+       {
+           cout << "\nOUTPUT TOTAL TIME OF PLAYLIST (IN SECONDS)" << endl;
+           cout << "Total time: " << list.totalTime() << " seconds" << endl;
+       }
+       else if (choice == 'o' || choice == 'O')
+       {
+           cout << endl << title << " - OUTPUT FULL PLAYLIST" << endl;
+           list.PrintList();
+       }
+       else
+       {
+           cout << "Invalid menu choice! Please try again." << endl;
+       }
+   }
+} // end of PrintMenu function
